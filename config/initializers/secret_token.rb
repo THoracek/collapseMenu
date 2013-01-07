7f3397816a2c5d7509f7f82ab21b407d44753f37
Thomas Horacek <tahoracek@avaya.com>
2013-01-07 13:57:59 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollapseMenu::Application.config.secret_token = '48a3477ac61bd34ea09be1d79551ca566428118db41c264aa0bb8cb8a2883287a3f92dba328a373d02a3d1c208f5da27d525de4f22193c201454f61c73ffa7a5'
